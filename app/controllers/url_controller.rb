291bd39a55cbf78c88dbed5908b2161e8fbbd0dd
Calvin Hawkes <calvinhawkes@gmail.com>
2013-08-19 15:53:32 -0400

    added comments to files about where to put methods

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -11,4 +11,14 @@ class UrlController < ApplicationController
   def show
     @url = params[:id]
   end
+
+
+=begin
+Creating a unique id will be something like this
+@uniqueObject = MyUniqueModel.new(modelParams)
+@uniqueObject.uuid_column = generate_uuid() # whatever method you use for generating your uuid
+while !uniqueObject.save # if at first it didn't save...
+    uniqueObject.uuid_column = generate_uuid() # try, try again
+end # should break out of the loop if the save succeeded.
+=end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -2,13 +2,11 @@ class Url < ActiveRecord::Base
   attr_accessible :url, :codename
   validates_uniqueness_of :codename
 
-  ###
-  #Generating unique id will be something like
-  #@uniqueObject = MyUniqueModel.new(modelParams)
-  #@uniqueObject.uuid_column = generate_uuid() #whatever method you use for generating your uuid
-  #while !uniqueObject.save # if at first it didn't save...
-      #uniqueObject.uuid_column = generate_uuid() # try, try again
-  #end # should break out of the loop if the save succeeded.
-  ###
+
+  #generates a codename from the given array of naughty words
+  #NOTE: Does not generate codenames uniqueness. Uniqueness is checked when object is saved
+  def generate_codename
+    #put code to generate codename here
+  end
 
  end
