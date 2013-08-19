d400f12a1d6f098aa3e5e7669f61d579c9a961a7
Calvin Hawkes <calvinhawkes@gmail.com>
2013-08-19 15:49:33 -0400

    give some example code on generating id

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,12 @@
 class Url < ActiveRecord::Base
   attr_accessible :url, :codename
   validates_uniqueness_of :codename
-end
+
+  ###
+  #Generating unique id will be something like
+  #@uniqueObject = MyUniqueModel.new(modelParams)
+  #@uniqueObject.uuid_column = generate_uuid() #whatever method you use for generating your uuid
+  #while !uniqueObject.save # if at first it didn't save...
+      #uniqueObject.uuid_column = generate_uuid() # try, try again
+  #end # should break out of the loop if the save succeeded.
+  ### end
